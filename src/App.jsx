bd9460d88f45718358f7cea5933318d3377aa1b7
piyush-kumar-kedia <piyushkumarkedia02221@gmail.com>
2026-03-28 11:32:55 +0530

    added vercel analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Analytics } from "@vercel/analytics/react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import About from "./components/About";
@@ -29,6 +30,7 @@ export default function App() {
         <Contact />
       </main>
       <Footer />
+      <Analytics />
     </div>
   );
 }
